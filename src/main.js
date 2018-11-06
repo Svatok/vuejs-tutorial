95ece3b4d48ed85dd8c85a8c91b05ab03f7e9925
Svatok <s_s_s@ua.fm>
2018-11-06 23:45:11 +0200

    Project setup

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
+import apolloClient from './utils/apolloClient'
 import router from './router'
+import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/scss/application.scss'
 
+Vue.use(VueApollo)
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
+  apolloProvider,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const mutations = {
+}
+
+export const actions = {
+}
+
+export const state = {
+}
+
+export default new Vuex.Store({
+  mutations,
+  actions,
+  state
+})
